fd873068cee8720b8eb915ba58da79e95cbe645d
Vardan Pogosian <vardan.pogosyan@gmail.com>
2020-02-12 19:13:20 +0300

    fix(shortcuts): refactord pause.js to use the new Cmd

--- shortcuts/pause.js
+++ shortcuts/pause.js
@@ -2,14 +2,13 @@
 /* PAUSE */
 
 (new EventDispatcher()).setHandler('f8', HYPER, () => {
-  let cmd = new Cmd();
 
-  cmd.osascript('tell application "System Events" to return name of first process whose frontmost is true', ({ output }) => {
+  Cmd.osascript('tell application "System Events" to return name of first process whose frontmost is true', ({ output }) => {
 
     const app = _.trim(output),
       isStopped = !Window.focused(); // If the app is stopped this will return undefined
 
-    cmd.shell(`killall -${isStopped ? 'CONT' : 'STOP'} -c '${app}'`);
+    Cmd.shell(`killall -${isStopped ? 'CONT' : 'STOP'} -c '${app}'`);
 
   });
 
