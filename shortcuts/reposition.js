5110b8da5e71aba625db6fac997cdd6fbf3156e2
Vardan Pogosian <vardan.pogosyan@gmail.com>
2025-07-28 11:41:33 +0500

    updated description of reposition.js

--- shortcuts/reposition.js
+++ shortcuts/reposition.js
@@ -1,7 +1,8 @@
 (new EventDispatcher()).setHandler('w', HYPER, () => {
-    // it emulates double click on a window title bar
-    // which has an effect of repositioning a window on the screen taking in account of dock
-    // after you switched form big screen size display to builtin display of mac book
+    // It emulates double click on a window title bar which has an effect of repositioning a window
+    // and maximize window to whole screen taking into account of dock size
+    // It's useful when you switched form big screen size display to builtin display of mac book and
+    // windows stay incorrect positioned and with size behind which gets behind the dock
     const script = `tell application "System Events"
                   perform action "AXZoomWindow" of (first button whose subrole is "AXFullScreenButton") \
                   of (first window whose subrole is "AXStandardWindow") \
